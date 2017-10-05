a1e74bb6cf28dff1e63fdcd3830e093c6f675e74
Cliff Rodgers <kliph@users.noreply.github.com>
2017-10-05 09:26:39 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject heap-for-proletarians "0.1.0-SNAPSHOT"
+  :description "Priority queue for clojure"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :repl-options {:init-ns dev.repl}
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]])
---
+++ src/heap_for_proletarians/core.clj
@@ -0,0 +1,1 @@
+(ns heap-for-proletarians.core)
